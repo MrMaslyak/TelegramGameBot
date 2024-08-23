7b7aac577bf6b83c7a0d4df9841b472f3eca1c78
Ilya Maslyanyi <maslanyjila@gmail.com>
2024-08-23 12:59:28 +0300

    rebranding branch

--- src/main/java/org/example/GameFunctions.java
+++ src/main/java/org/example/GameFunctions.java
@@ -19,84 +19,19 @@ public class GameFunctions {
     }
 
     public void startGame(List<User> users) {
-        if (users.size() >= 2) {
-            messageSender.sendText(users.get(0).getChatID(), "Игра на Удачу началась! \n" + users.get(0).getName() + " VS " + users.get(1).getName());
-            messageSender.sendText(users.get(1).getChatID(), "Игра на Удачу началась! \n" + users.get(0).getName() + " VS " + users.get(1).getName());
-            messageSender.sendText(users.get(0).getChatID(), "\uD83D\uDC9AПрячь свой алмаз удачи\uD83D\uDC9A");
-            messageSender.sendText(users.get(1).getChatID(), "\uD83D\uDE42\u200D↕\uFE0F Жди \uD83D\uDE42\u200D↕\uFE0F");
-            sendHideButtons(users.get(0).getChatID());
-            isPlay = false;
-        }
+
     }
 
     public void currentGame(Update update, List<User> users) {
-        String receivedData = update.getCallbackQuery().getData();
 
-        if (!isHidden) {
-            answer = receivedData;
-            messageSender.sendText(users.get(0).getChatID(), "\uD83E\uDE75 Жди, пока аппонент выберет в какой ящик ты спрятал \uD83E\uDE75");
-            messageSender.sendText(users.get(1).getChatID(), "\uD83E\uDE75 Ищи Алмаз! \uD83E\uDE75");
-            sendSearchButtons(users.get(1).getChatID());
-            isHidden = true;
-        } else {
-            if (answer.equals(receivedData)) {
-                messageSender.sendText(users.get(1).getChatID(), "Угадал! \nТы нашел алмаз! ❤\uFE0F\u200D\uD83D\uDD25");
-                messageSender.sendText(users.get(0).getChatID(), "<----> <----> <--->");
-                messageSender.sendText(users.get(0).getChatID(), "Ты проиграл! ☹\uFE0F");
-            } else {
-                messageSender.sendText(users.get(1).getChatID(), "Неправильно, алмаз был спрятан в " + answer + "!");
-                messageSender.sendText(users.get(1).getChatID(), "Ты проиграл! ☹\uFE0F");
-                messageSender.sendText(users.get(0).getChatID(), "<----> <----> <--->");
-                messageSender.sendText(users.get(0).getChatID(), "\uD83D\uDCA5 Ты победил! \uD83D\uDCA5");
-            }
-            offerNewGame(users);
-        }
     }
 
     private void sendHideButtons(long chatID) {
-        InlineKeyboardButton button1 = InlineKeyboardButton.builder()
-                .text("Ящик 1").callbackData("Ящик 1")
-                .build();
 
-        InlineKeyboardButton button2 = InlineKeyboardButton.builder()
-                .text("Ящик 2").callbackData("Ящик 2")
-                .build();
-
-        InlineKeyboardButton button3 = InlineKeyboardButton.builder()
-                .text("Ящик 3").callbackData("Ящик 3")
-                .build();
-
-        List<InlineKeyboardButton> row = new ArrayList<>();
-        row.add(button1);
-        row.add(button2);
-        row.add(button3);
-
-        InlineKeyboardMarkup keyboardMarkup = new InlineKeyboardMarkup();
-        keyboardMarkup.setKeyboard(Collections.singletonList(row));
-        messageSender.sendMenu(chatID, "❓\uFE0F Выберите, куда спрятать алмаз: ", keyboardMarkup);
     }
 
     private void sendSearchButtons(long chatID) {
-        InlineKeyboardButton button1 = InlineKeyboardButton.builder()
-                .text("Ящик 1").callbackData("Ящик 1")
-                .build();
 
-        InlineKeyboardButton button2 = InlineKeyboardButton.builder()
-                .text("Ящик 2").callbackData("Ящик 2")
-                .build();
-
-        InlineKeyboardButton button3 = InlineKeyboardButton.builder()
-                .text("Ящик 3").callbackData("Ящик 3")
-                .build();
-
-        List<InlineKeyboardButton> row = new ArrayList<>();
-        row.add(button1);
-        row.add(button2);
-        row.add(button3);
-
-        InlineKeyboardMarkup keyboardMarkup = new InlineKeyboardMarkup();
-        keyboardMarkup.setKeyboard(Collections.singletonList(row));
-        messageSender.sendMenu(chatID, "Попробуй найти алмаз:", keyboardMarkup);
     }
 
     public void resetGame(List<User> users) {
